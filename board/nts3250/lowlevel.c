1910235d3b19e5cf9b1d2d3c5f77a90ca1ebfe58
Athurg Gooth <athurg@gooth.cn>
2010-12-30 13:52:07 +0800

    修复看门狗失效的问题（增加看门狗所在的EMC接口初始化）

--- board/nts3250/lowlevel.c
+++ board/nts3250/lowlevel.c
@@ -1,4 +1,37 @@
 #include <lpc3250.h>
+/* EMC Static Configuration */
+// tHCLK = 10ns
+#define EMC_CS0_WIDTH		EMC_STC_MEMWIDTH_16
+#define EMC_CS0_CS_TO_WE	4
+#define EMC_CS0_CS_TO_OE	4
+#define EMC_CS0_CS_TO_RD	18
+#define EMC_CS0_CS_TO_RD_PM	0
+#define EMC_CS0_CS_TO_WR	18
+#define EMC_CS0_OE_TO_HIZ	4
+
+#define EMC_CS1_WIDTH		EMC_STC_MEMWIDTH_16
+#define EMC_CS1_CS_TO_WE	4
+#define EMC_CS1_CS_TO_OE	4
+#define EMC_CS1_CS_TO_RD	18
+#define EMC_CS1_CS_TO_RD_PM	0
+#define EMC_CS1_CS_TO_WR	18
+#define EMC_CS1_OE_TO_HIZ	4
+
+#define EMC_CS2_WIDTH		EMC_STC_MEMWIDTH_8
+#define EMC_CS2_CS_TO_WE	6
+#define EMC_CS2_CS_TO_OE	6
+#define EMC_CS2_CS_TO_RD	20
+#define EMC_CS2_CS_TO_RD_PM	0
+#define EMC_CS2_CS_TO_WR	20
+#define EMC_CS2_OE_TO_HIZ	4
+
+#define EMC_CS3_WIDTH		EMC_STC_MEMWIDTH_16
+#define EMC_CS3_CS_TO_WE	6
+#define EMC_CS3_CS_TO_OE	6
+#define EMC_CS3_CS_TO_RD	20
+#define EMC_CS3_CS_TO_RD_PM	0
+#define EMC_CS3_CS_TO_WR	20
+#define EMC_CS3_OE_TO_HIZ	4
 
 /*
  * EMC SDRAM Configuration
@@ -37,7 +70,6 @@
 /***********************************************************************
  Clock
 ***********************************************************************/
-
 void init_clock(void)
 {
 	unsigned int tmp;
@@ -56,7 +88,7 @@ void init_clock(void)
 }
 
 
-void sdram_adjust_timing(void)
+inline void sdram_adjust_timing(void)
 {
 	EMC->emcdynamictrp     = EMC_DYN_PRE_CMD_PER(SDRAM_TRP - 1);
 	EMC->emcdynamictras    = EMC_DYN_ACTPRE_CMD_PER(SDRAM_TRAS - 1);
@@ -71,7 +103,7 @@ void sdram_adjust_timing(void)
 	EMC->emcdynamicrefresh = EMC_DYN_REFRESH_IVAL(SDRAM_REFRESH);
 }
 
-void init_sdram(void)
+void setup_sdram(void)
 {
 	volatile unsigned int tmp, tmp32;
 	CLKPWR->clkpwr_sdramclk_ctrl = CLKPWR_SDRCLK_HCLK_DLY(0x0E);
@@ -94,8 +126,69 @@ void init_sdram(void)
 	EMC->emcdynamiccontrol = tmp;
 }
 
+void setup_stc(void)
+{
+	EMC->emcstatic_regs[0].emcstaticconfig  = (EMC_CS0_WIDTH | EMC_STC_BLS_EN_BIT);
+	EMC->emcstatic_regs[0].emcstaticwaitwen = (EMC_CS0_CS_TO_WE - 1);
+	EMC->emcstatic_regs[0].emcstaticwait0en = (EMC_CS0_CS_TO_OE);
+	EMC->emcstatic_regs[0].emcstaticwaitrd  = (EMC_CS0_CS_TO_RD - 1);
+	EMC->emcstatic_regs[0].emcstaticpage    = (EMC_CS0_CS_TO_RD_PM - 1);
+	EMC->emcstatic_regs[0].emcstaticwr      = (EMC_CS0_CS_TO_WR - 2);
+	EMC->emcstatic_regs[0].emcstaticturn    = (EMC_CS0_OE_TO_HIZ - 1);
+	EMC->emcstatic_regs[1].emcstaticconfig  = (EMC_CS1_WIDTH | EMC_STC_BLS_EN_BIT);
+	EMC->emcstatic_regs[1].emcstaticwaitwen = (EMC_CS1_CS_TO_WE - 1);
+	EMC->emcstatic_regs[1].emcstaticwait0en = (EMC_CS1_CS_TO_OE);
+	EMC->emcstatic_regs[1].emcstaticwaitrd  = (EMC_CS1_CS_TO_RD - 1);
+	EMC->emcstatic_regs[1].emcstaticpage    = (EMC_CS1_CS_TO_RD_PM - 1);
+	EMC->emcstatic_regs[1].emcstaticwr      = (EMC_CS1_CS_TO_WR - 2);
+	EMC->emcstatic_regs[1].emcstaticturn    = (EMC_CS1_OE_TO_HIZ - 1);
+	EMC->emcstatic_regs[2].emcstaticconfig  = (EMC_CS2_WIDTH | EMC_STC_BLS_EN_BIT);
+	EMC->emcstatic_regs[2].emcstaticwaitwen = (EMC_CS2_CS_TO_WE - 1);
+	EMC->emcstatic_regs[2].emcstaticwait0en = (EMC_CS2_CS_TO_OE);
+	EMC->emcstatic_regs[2].emcstaticwaitrd  = (EMC_CS2_CS_TO_RD - 1);
+	EMC->emcstatic_regs[2].emcstaticpage    = (EMC_CS2_CS_TO_RD_PM - 1);
+	EMC->emcstatic_regs[2].emcstaticwr      = (EMC_CS2_CS_TO_WR - 2);
+	EMC->emcstatic_regs[2].emcstaticturn    = (EMC_CS2_OE_TO_HIZ - 1);
+	EMC->emcstatic_regs[3].emcstaticconfig  = (EMC_CS3_WIDTH | EMC_STC_BLS_EN_BIT);
+	EMC->emcstatic_regs[3].emcstaticwaitwen = (EMC_CS3_CS_TO_WE - 1);
+	EMC->emcstatic_regs[3].emcstaticwait0en = (EMC_CS3_CS_TO_OE);
+	EMC->emcstatic_regs[3].emcstaticwaitrd  = (EMC_CS3_CS_TO_RD - 1);
+	EMC->emcstatic_regs[3].emcstaticpage    = (EMC_CS3_CS_TO_RD_PM - 1);
+	EMC->emcstatic_regs[3].emcstaticwr      = (EMC_CS3_CS_TO_WR - 2);
+	EMC->emcstatic_regs[3].emcstaticturn    = (EMC_CS3_OE_TO_HIZ - 1);
+}
+
+
+void init_emc(void)
+{
+	/* Mirror IRAM at address 0x0 */
+	CLKPWR->clkpwr_bootmap = CLKPWR_BOOTMAP_SEL_BIT;
+
+	/* Disable buffers in AHB ports */
+	EMC->emcahn_regs [0].emcahbcontrol = 0;
+	EMC->emcahn_regs [3].emcahbcontrol = 0;
+	EMC->emcahn_regs [4].emcahbcontrol = 0;
+
+	/* Enable port timeouts */
+	EMC->emcahn_regs [0].emcahbtimeout = EMC_AHB_SET_TIMEOUT(400);
+	EMC->emcahn_regs [3].emcahbtimeout = EMC_AHB_SET_TIMEOUT(400);
+	EMC->emcahn_regs [4].emcahbtimeout = EMC_AHB_SET_TIMEOUT(400);
+
+	/* Setup Static Memeory Controller */
+	setup_stc();
+
+	/* Setup SDRAM Controller */
+	setup_sdram();
+
+	/* Enable buffers in AHB ports */
+	EMC->emcahn_regs [0].emcahbcontrol = EMC_AHB_PORTBUFF_EN;
+	EMC->emcahn_regs [3].emcahbcontrol = EMC_AHB_PORTBUFF_EN;
+	EMC->emcahn_regs [4].emcahbcontrol = EMC_AHB_PORTBUFF_EN;
+}
+
+
 void  nts3250_lowlevel_init(void)
 {
 	init_clock();
-	init_sdram();
+	init_emc();
 }
