b7ac1c61442733cf44335c578413256b2c4177b7
Athurg Gooth <athurg@gooth.cn>
2010-12-31 09:07:06 +0800

    增加代码注释

--- board/nts3250/lowlevel.c
+++ board/nts3250/lowlevel.c
@@ -1,5 +1,28 @@
+/*
+::::    :::: ::::::::::::    .::::::    Company    : NTS-intl
+ :::     ::   ::  ::  ::   ::      ::   Author     : Athurg.Feng
+ ::::    ::       ::        ::          Maintainer : Athurg.Feng
+ :: ::   ::       ::         ::         Project    : 
+ ::  ::  ::       ::           :::      FileName   : lowlevel.c
+ ::   :: ::       ::             ::     Generate   : 
+ ::    ::::       ::       ::      ::   Update     : 2010-12-30 15:35:31
+::::    :::     ::::::      ::::::::    Version    : 0.0.1
+
+Description:
+		本文件包含的是LPC32xx硬件低级初始化代码，用于配置时钟、EMC
+	控制器（SDRAM）等设备。
+		nts3250_lowlevel_init()函数是本文件的入口函数，其他函数被此
+	函数调用。nts3250_lowlevel_init()被lowlevel_init.S中的汇编语言调用。
+	主要作用是在uboot启动的stage1阶段初始化时钟和SDRAM，为后面的relocate
+	复制uboot到SDRAM作准备。
+
+Changelog:
+	V0.0.1	版本发布
+*/
 #include <lpc3250.h>
-/* EMC Static Configuration */
+
+//EMC配置，具体配置含义及内容参考《BSP详细设计报告》
+/* 静态存储控制器相关配置 */
 // tHCLK = 10ns
 #define EMC_CS0_WIDTH		EMC_STC_MEMWIDTH_16
 #define EMC_CS0_CS_TO_WE	4
@@ -34,7 +57,7 @@
 #define EMC_CS3_OE_TO_HIZ	4
 
 /*
- * EMC SDRAM Configuration
+ * EMC SDRAM相关配置
  *
  * IS42S16160C
  * 4,194,304 Words x 4 Banks x 16 Bits Organization
@@ -67,12 +90,17 @@
 #define SDRAM_TCDLR	1 // 1clk
 #define SDRAM_REFRESH	650 // (64ms/8192/10ns)/1.2 = 650clk
 
-/***********************************************************************
- Clock
-***********************************************************************/
+/*
+ * 	时钟初始化
+ *
+ * 说明：
+ * 	配置值及含义参考LPC32XX用户手册及《BSP详细设计报告》
+ */
+
 void init_clock(void)
 {
 	unsigned int tmp;
+
 	CLKPWR->clkpwr_sysclk_ctrl = CLKPWR_SYSCTRL_BP_TRIG(0x50);
 	CLKPWR->clkpwr_hclkpll_ctrl = (CLKPWR_HCLKPLL_POWER_UP |
 		CLKPWR_HCLKPLL_POSTDIV_BYPASS | CLKPWR_HCLKPLL_PREDIV_PLUS1(1) |
@@ -88,7 +116,7 @@ void init_clock(void)
 }
 
 
-inline void sdram_adjust_timing(void)
+static inline void sdram_adjust_timing(void)
 {
 	EMC->emcdynamictrp     = EMC_DYN_PRE_CMD_PER(SDRAM_TRP - 1);
 	EMC->emcdynamictras    = EMC_DYN_ACTPRE_CMD_PER(SDRAM_TRAS - 1);
@@ -102,19 +130,33 @@ inline void sdram_adjust_timing(void)
 	EMC->emcdynamictcdlr   = EMC_DYN_LASTDIN_CMD_TIME(SDRAM_TCDLR - 1);
 	EMC->emcdynamicrefresh = EMC_DYN_REFRESH_IVAL(SDRAM_REFRESH);
 }
-
-void setup_sdram(void)
+/*
+ * 	EMC动态总线配置
+ *
+ * 说明：
+ * 	用于初始化动态存储设备控制器（Dynamic Memory Control）。
+ * 	SDRAM联接在该控制器上。
+ */
+static inline void setup_sdram(void)
 {
 	volatile unsigned int tmp, tmp32;
+
+	//设置HCLK延时
 	CLKPWR->clkpwr_sdramclk_ctrl = CLKPWR_SDRCLK_HCLK_DLY(0x0E);
+
 	EMC->emccontrol = EMC_DYN_SDRAM_CTRL_EN;
 	EMC->emcconfig = 0;
+
 	EMC->emcdynamicconfig0 = (SDRAM_CFG_MD<<7) | EMC_DYN_DEV_LP_SDR_SDRAM;
+
 	EMC->emcdynamicrascas0 = (EMC_SET_CAS_IN_HALF_CYCLES(SDRAM_CAS*2) |
 			EMC_SET_RAS_IN_CYCLES(SDRAM_RAS));
 	EMC->emcdynamicreadconfig = (EMC_SDR_CLK_NODLY_CMD_DEL |
 			EMC_SDR_READCAP_POS_POL | EMC_SDR_CLK_DLY_CMD_NODELY);
+
+	//根据新HCLK配置重新调整SDRAM
 	sdram_adjust_timing();
+
 	tmp = (EMC_DYN_CLK_ALWAYS_ON | EMC_DYN_CLKEN_ALWAYS_ON |
 			EMC_DYN_DIS_INV_MEMCLK);
 	EMC->emcdynamiccontrol = (tmp | EMC_DYN_NOP_MODE);
@@ -126,8 +168,17 @@ void setup_sdram(void)
 	EMC->emcdynamiccontrol = tmp;
 }
 
-void setup_stc(void)
+/*
+ * 	EMC静态总线配置
+ *
+ * 说明：
+ * 	用于初始化静态存储设备控制器（Static Memory Control）。
+ * 	包括NorFlash、CPLD、FPGA等设备都是联接在该控制器上。
+ * 	具体初始化值及作用参考本文件头的相关宏定义
+ */
+static inline void setup_stc(void)
 {
+	//EMC_CS0对应的片选空间
 	EMC->emcstatic_regs[0].emcstaticconfig  = (EMC_CS0_WIDTH | EMC_STC_BLS_EN_BIT);
 	EMC->emcstatic_regs[0].emcstaticwaitwen = (EMC_CS0_CS_TO_WE - 1);
 	EMC->emcstatic_regs[0].emcstaticwait0en = (EMC_CS0_CS_TO_OE);
@@ -135,6 +186,8 @@ void setup_stc(void)
 	EMC->emcstatic_regs[0].emcstaticpage    = (EMC_CS0_CS_TO_RD_PM - 1);
 	EMC->emcstatic_regs[0].emcstaticwr      = (EMC_CS0_CS_TO_WR - 2);
 	EMC->emcstatic_regs[0].emcstaticturn    = (EMC_CS0_OE_TO_HIZ - 1);
+
+	//EMC_CS1对应的片选空间
 	EMC->emcstatic_regs[1].emcstaticconfig  = (EMC_CS1_WIDTH | EMC_STC_BLS_EN_BIT);
 	EMC->emcstatic_regs[1].emcstaticwaitwen = (EMC_CS1_CS_TO_WE - 1);
 	EMC->emcstatic_regs[1].emcstaticwait0en = (EMC_CS1_CS_TO_OE);
@@ -142,6 +195,8 @@ void setup_stc(void)
 	EMC->emcstatic_regs[1].emcstaticpage    = (EMC_CS1_CS_TO_RD_PM - 1);
 	EMC->emcstatic_regs[1].emcstaticwr      = (EMC_CS1_CS_TO_WR - 2);
 	EMC->emcstatic_regs[1].emcstaticturn    = (EMC_CS1_OE_TO_HIZ - 1);
+
+	//EMC_CS2对应的片选空间
 	EMC->emcstatic_regs[2].emcstaticconfig  = (EMC_CS2_WIDTH | EMC_STC_BLS_EN_BIT);
 	EMC->emcstatic_regs[2].emcstaticwaitwen = (EMC_CS2_CS_TO_WE - 1);
 	EMC->emcstatic_regs[2].emcstaticwait0en = (EMC_CS2_CS_TO_OE);
@@ -149,6 +204,8 @@ void setup_stc(void)
 	EMC->emcstatic_regs[2].emcstaticpage    = (EMC_CS2_CS_TO_RD_PM - 1);
 	EMC->emcstatic_regs[2].emcstaticwr      = (EMC_CS2_CS_TO_WR - 2);
 	EMC->emcstatic_regs[2].emcstaticturn    = (EMC_CS2_OE_TO_HIZ - 1);
+
+	//EMC_CS3对应的片选空间
 	EMC->emcstatic_regs[3].emcstaticconfig  = (EMC_CS3_WIDTH | EMC_STC_BLS_EN_BIT);
 	EMC->emcstatic_regs[3].emcstaticwaitwen = (EMC_CS3_CS_TO_WE - 1);
 	EMC->emcstatic_regs[3].emcstaticwait0en = (EMC_CS3_CS_TO_OE);
@@ -161,26 +218,26 @@ void setup_stc(void)
 
 void init_emc(void)
 {
-	/* Mirror IRAM at address 0x0 */
+	/* 将地址0x00映射到IRAM中（初始为IROM） */
 	CLKPWR->clkpwr_bootmap = CLKPWR_BOOTMAP_SEL_BIT;
 
-	/* Disable buffers in AHB ports */
+	/* 关闭AHB端口缓冲 */
 	EMC->emcahn_regs [0].emcahbcontrol = 0;
 	EMC->emcahn_regs [3].emcahbcontrol = 0;
 	EMC->emcahn_regs [4].emcahbcontrol = 0;
 
-	/* Enable port timeouts */
+	/* 开启端口超时 */
 	EMC->emcahn_regs [0].emcahbtimeout = EMC_AHB_SET_TIMEOUT(400);
 	EMC->emcahn_regs [3].emcahbtimeout = EMC_AHB_SET_TIMEOUT(400);
 	EMC->emcahn_regs [4].emcahbtimeout = EMC_AHB_SET_TIMEOUT(400);
 
-	/* Setup Static Memeory Controller */
+	/* 配置静态存储控制器（NorFlash） */
 	setup_stc();
 
-	/* Setup SDRAM Controller */
+	/* 配置动态存储控制器（SDRAM） */
 	setup_sdram();
 
-	/* Enable buffers in AHB ports */
+	/* 重新开启AHB端口缓冲 */
 	EMC->emcahn_regs [0].emcahbcontrol = EMC_AHB_PORTBUFF_EN;
 	EMC->emcahn_regs [3].emcahbcontrol = EMC_AHB_PORTBUFF_EN;
 	EMC->emcahn_regs [4].emcahbcontrol = EMC_AHB_PORTBUFF_EN;
